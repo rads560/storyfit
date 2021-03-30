920411977d375b3751534f9ec27d73c21decea19
Justin Wilford <jwilford@usc.edu>
2021-03-30 14:13:18 -0500

    Added support for programming 3 different boards:Arduino Uno, EE459_ATMega328P, and Nucleo-F401RE

--- sensor_test/sensor_test.ino
+++ sensor_test/sensor_test.ino
@@ -9,11 +9,84 @@
 #include "MAX30105.h"
 #include "heartRate.h"
 
-// For the breakout board, you can use any 2 or 3 pins.
-// These pins will also work for the 1.8" TFT shield.
-#define TFT_CS        10
-#define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
-#define TFT_DC         8
+//=====================================================================================
+//    Pin Definitions
+//-------------------------------------------------------------------------------------
+// Arduino Uno
+#ifdef ARDUINO_AVR_UNO
+  // I2C Configuration is automatic
+  //    SDA => Pin 18 (A4/D18)
+  //    SCL => Pin 19 (A5/D19)
+  // SPI Configuration is automatic
+  //    SS => Pin 10 (D10)
+  //    MOSI => Pin 11 (D11)
+  //    MISO => Pin 12 (D12)
+  //    SCK => Pin 13 (D13)
+  #define TFT_CS        10
+  #define TFT_RST       9 // Or set to -1 and connect to Arduino RESET pin
+  #define TFT_DC        8
+  //#define TFT_MOSI 11  // Data out
+  //#define TFT_SCLK 13  // Clock out
+  #define BUTTON_LEFT   A0
+  #define BUTTON_CENTER A1
+  #define BUTTON_RIGHT  A2
+  #define LED_R         6
+  #define LED_G         5
+  #define LED_B         4
+#endif
+
+// EE459 ATMega328P
+#ifdef ARDUINO_EE459_ATMEGA328P
+  #ifndef SMALL_MODE
+    #define SMALL_MODE
+  #endif
+  // I2C Configuration is automatic
+  //    SDA => Pin 18 (Physical 27)
+  //    SCL => Pin 19 (Physical 28)
+  // SPI Configuration is automatic
+  //    SS => Pin 10 (Physical 16)
+  //    MOSI => Pin 11 (Physical 17)
+  //    MISO => Pin 12 (Physical 18)
+  //    SCK => Pin 13 (Physical 19)
+  #define TFT_CS        10    // Physical 16
+  #define TFT_RST       9     // Physical 15
+  #define TFT_DC        8     // Physical 14
+  //#define TFT_MOSI 11  // Data out
+  //#define TFT_SCLK 13  // Clock out
+  #define BUTTON_LEFT   A0    // Physical 23
+  #define BUTTON_CENTER A1    // Physical 24
+  #define BUTTON_RIGHT  A2    // Physical 25
+  #define LED_R         6     // Physical 12
+  #define LED_G         5     // Physical 11
+  #define LED_B         4     // Physical 6
+#endif
+
+// Nucleo-F401RE
+#ifdef ARDUINO_NUCLEO_F401RE
+  // I2C Configuration is automatic
+  //    SDA => Pin 14 (PB9/D14)
+  //    SCL => Pin 15 (PB8/D15)
+  // SPI Configuration is automatic
+  //    SS => Pin 10 (PB6/D10)
+  //    SS1 => Pin 4 (PB5/D4)
+  //    SS2 => Pin 7 (PA8/D7)
+  //    SS3 => Pin 8 (PA9/D8)
+  //    MOSI => Pin 11 (D11)
+  //    MISO => Pin 12 (D12)
+  //    SCK => Pin 13 (PA5/D13)
+  #define TFT_CS        10
+  #define TFT_RST       9     // Or set to -1 and connect to Arduino RESET pin
+  #define TFT_DC        8
+  //#define TFT_MOSI 11  // Data out
+  //#define TFT_SCLK 13  // Clock out
+  #define BUTTON_LEFT   46    //A0
+  #define BUTTON_CENTER 47    //A1
+  #define BUTTON_RIGHT  48    //A2
+  #define LED_R         6
+  #define LED_G         5
+  #define LED_B         4
+#endif
+//=====================================================================================
 
 // OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
 // to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
@@ -35,14 +108,9 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 Adafruit_BME280 bme; // I2C
 
-// Buttons!
-const int BUTTON_LEFT = A0;
-const int BUTTON_CENTER = A1;
-const int BUTTON_RIGHT = A2;
-
 // Heartrate monitor
 MAX30105 particleSensor;
-const byte RATE_SIZE = 32; //Increase this for more averaging. 4 is good.
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
@@ -57,11 +125,17 @@ void setup() {
   pinMode(BUTTON_LEFT, INPUT);
   pinMode(BUTTON_CENTER, INPUT);
   pinMode(BUTTON_RIGHT, INPUT);
+
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
   
   // put your setup code here, to run once:
+#ifndef SMALL_MODE
   Serial.begin(9600);
   while(!Serial);    // time to get serial running
   Serial.print(F("Story Fit Sensor Test"));
+#endif
 
   // Use this initializer (uncomment) if using a 1.3" or 1.54" 240x240 TFT:
   tft.init(240, 240);           // Init ST7789 240x240
@@ -69,26 +143,32 @@ void setup() {
   // SPI speed defaults to SPI_DEFAULT_FREQ defined in the library, you can override it here
   // Note that speed allowable depends on chip and quality of wiring, if you go too fast, you
   // may end up with a black screen some times, or all the time.
-  //tft.setSPISpeed(40000000);
+  //tft.setSPISpeed(F_CPU/2);
 
   // Setup BME280
   unsigned status = bme.begin();  
   // You can also pass in a Wire library object like &Wire2
   // status = bme.begin(0x76, &Wire2)
   if (!status) {
+    #ifndef SMALL_MODE
       Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
       Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
       Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
       Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
       Serial.print("        ID of 0x60 represents a BME 280.\n");
       Serial.print("        ID of 0x61 represents a BME 680.\n");
+    #endif
+      digitalWrite(LED_R, HIGH);
       while (1) delay(10);
   }
 
   // Initialize heartrate sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
+  #ifndef SMALL_MODE
     Serial.println("MAX30105 was not found. Please check wiring/power. ");
+  #endif
+    digitalWrite(LED_G, HIGH);
     while (1);
   }
 
@@ -102,7 +182,8 @@ void setup() {
   lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
   lis3mdl.setRange(LIS3MDL_RANGE_16_GAUSS);
   
-  Serial.println(F("Initialized"));
+//  Serial.println(F("Initialized"));
+  digitalWrite(LED_B, HIGH);
 
   // StoryFit Splash
   tft.fillScreen(ST77XX_BLACK);
@@ -114,6 +195,7 @@ void setup() {
     "StoryFit\nSensor Test"
     );
   delay(2000);
+  digitalWrite(LED_B, LOW);
 
   // Clear
   tft.fillScreen(ST77XX_BLACK);
@@ -180,11 +262,11 @@ float getHeart() {
   return beatsPerMinute;
 }
 
-int getLight() {
+uint8_t getLight() {
   return 0;
 }
 
-int getProx() {
+uint8_t getProx() {
   return 0;
 }
 
@@ -213,9 +295,9 @@ float s_compass[3];
 bool s_compass_ch;
 float s_heart;
 bool s_heart_ch;
-int s_light;
+uint8_t s_light;
 bool s_light_ch;
-int s_prox;
+uint8_t s_prox;
 bool s_prox_ch;
 bool b_left;
 bool b_center;
@@ -339,9 +421,11 @@ void generate_output() {
   // Left button pressed
   if (b_left) {
     tft.setTextColor(ST77XX_YELLOW);
+    digitalWrite(LED_R, HIGH);
   }
   else {
     tft.setTextColor(ST77XX_WHITE);
+    digitalWrite(LED_R, LOW);
   }
   tft.setCursor(0, 14*TEXT_CHAR_HEIGHT);
   tft.print("B_L");
@@ -349,9 +433,11 @@ void generate_output() {
   // Center button pressed
   if (b_center) {
     tft.setTextColor(ST77XX_YELLOW);
+    digitalWrite(LED_G, HIGH);
   }
   else {
     tft.setTextColor(ST77XX_WHITE);
+    digitalWrite(LED_G, LOW);
   }
   tft.setCursor(8*TEXT_CHAR_WIDTH, 14*TEXT_CHAR_HEIGHT);
   tft.print("B_C");
@@ -359,9 +445,11 @@ void generate_output() {
   // Right button pressed
   if (b_right) {
     tft.setTextColor(ST77XX_YELLOW);
+    digitalWrite(LED_B, HIGH);
   }
   else {
     tft.setTextColor(ST77XX_WHITE);
+    digitalWrite(LED_B, LOW);
   }
   tft.setCursor(16*TEXT_CHAR_WIDTH, 14*TEXT_CHAR_HEIGHT);
   tft.print("B_R");
